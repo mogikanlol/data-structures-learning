8b41afc9365c4feb262c594d04fd40d2fd4f417d
unknown <Dmitry Nikolaev>
2022-07-27 23:20:35 +0400

    Add test case for Stack

--- src/main/java/com/example/app/MStack.java
+++ src/main/java/com/example/app/MStack.java
@@ -1,5 +1,8 @@
 package com.example.app;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import java.util.EmptyStackException;
 
 public class MStack<E> {
@@ -57,27 +60,46 @@ public class MStack<E> {
         }
     }
 
-    public static void main(String[] args) {
+    @Test
+    void test() {
         MStack<String> stack = new MStack<>();
 
+        Assertions.assertTrue(stack.isEmpty());
+
         stack.push("H");
+        Assertions.assertEquals("H", stack.peek());
+        Assertions.assertFalse(stack.isEmpty());
+
         stack.push("E");
+        Assertions.assertEquals("E", stack.peek());
+        Assertions.assertFalse(stack.isEmpty());
+
         stack.push("L");
+        Assertions.assertEquals("L", stack.peek());
+        Assertions.assertFalse(stack.isEmpty());
+
         stack.push("L");
+        Assertions.assertEquals("L", stack.peek());
+        Assertions.assertFalse(stack.isEmpty());
+
         stack.push("O");
+        Assertions.assertEquals("O", stack.peek());
+        Assertions.assertFalse(stack.isEmpty());
 
-        stack.print();
+        Assertions.assertEquals("O", stack.pop());
+        Assertions.assertFalse(stack.isEmpty());
+        Assertions.assertEquals("L", stack.pop());
+        Assertions.assertFalse(stack.isEmpty());
+        Assertions.assertEquals("L", stack.pop());
+        Assertions.assertFalse(stack.isEmpty());
+        Assertions.assertEquals("E", stack.pop());
+        Assertions.assertFalse(stack.isEmpty());
+        Assertions.assertEquals("H", stack.pop());
 
-        System.out.println("Pop: " + stack.pop());
-        System.out.println("Pop: " + stack.pop());
-        System.out.println("Pop: " + stack.pop());
-        System.out.println("Pop: " + stack.pop());
-        System.out.println("Pop: " + stack.pop());
-        try {
-            System.out.println("Pop: " + stack.pop());
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        stack.print();
+        Assertions.assertTrue(stack.isEmpty());
+
+        Assertions.assertThrows(Exception.class, stack::pop);
+        Assertions.assertThrows(Exception.class, stack::peek);
     }
+
 }
