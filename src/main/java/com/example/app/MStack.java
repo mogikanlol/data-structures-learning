84c027937168dd36f44a5ba790ae87f95a2d83ec
unknown <Dmitry Nikolaev>
2022-07-27 18:52:35 +0400

    Update stack implementation

--- src/main/java/com/example/app/MStack.java
+++ src/main/java/com/example/app/MStack.java
@@ -1,45 +1,59 @@
 package com.example.app;
 
+import java.util.EmptyStackException;
+
 public class MStack<E> {
 
-    private MNode<E> head;
+    private MNode<E> top;
 
     public void push(E e) {
-        MNode<E> node = new MNode<>();
-        node.value = e;
-        node.next = null;
-
-        if (head == null) {
-            head = node;
-        } else {
-            MNode<E> oldHead = this.head;
-            this.head = node;
-            node.next = oldHead;
-        }
+        MNode<E> node = new MNode<>(e);
+        node.next = top;
+        top = node;
     }
 
     public E pop() {
-        MNode<E> oldHead = this.head;
-        if (oldHead != null) {
-            this.head = oldHead.next;
-            return oldHead.value;
-        } else {
-            return null;
+        if (top == null) {
+            throw new EmptyStackException();
         }
+
+        E val = top.value;
+        top = top.next;
+        return val;
     }
 
-    private static class MNode<E> {
-        private E value;
-        private MNode<E> next;
+    public E peek() {
+        if (top == null) {
+            throw new EmptyStackException();
+        }
+
+        return top.value;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
     }
 
     public void print() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+            return;
+        }
         System.out.println("Printing stack from the top");
-        MNode<E> currentNode = this.head;
+        MNode<E> currentNode = this.top;
         while (currentNode != null) {
             System.out.println(currentNode.value);
 
             currentNode = currentNode.next;
+        }
+    }
+
+    private static class MNode<E> {
+        private final E value;
+        private MNode<E> next;
+
+        public MNode(E value) {
+            this.value = value;
         }
     }
 
@@ -59,8 +73,11 @@ public class MStack<E> {
         System.out.println("Pop: " + stack.pop());
         System.out.println("Pop: " + stack.pop());
         System.out.println("Pop: " + stack.pop());
-        System.out.println("Pop: " + stack.pop());
-
+        try {
+            System.out.println("Pop: " + stack.pop());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
         stack.print();
     }
 }
